e0ac2da786bf07c6557ac601326d3628b3861c99
MonkeyFirst <monkey3dcg@gmail.com>
2015-01-11 18:24:44 +0500

    add normalizedtime property to T-button panel
    by default it off

--- add-on source/MarkersExport.py
+++ add-on source/MarkersExport.py
@@ -11,6 +11,7 @@ import struct
 import sys
 import mathutils
 from math import radians
+from bpy.props import (BoolProperty)
 
           
 class MarkersExporter(bpy.types.Operator):
@@ -18,6 +19,7 @@ class MarkersExporter(bpy.types.Operator):
     bl_label = "Urho3D Markers Create"     # display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
 
+    normalizeMarkers = BoolProperty( name="Normalize markers", description="Normalize markers", default=False )
    
     def SaveLocalActionMarkes(self, context):
         
@@ -26,12 +28,15 @@ class MarkersExporter(bpy.types.Operator):
         print ("render fps:{0}".format(fps))
         
         
+        
+        
         for i, action in enumerate(bpy.data.actions):
             print ("index: {0} action name: {1} have {2} markers".format(i, action.name, len(action.pose_markers)))
             print ("frame range {0} to {1}".format(action.frame_range[0], action.frame_range[1]))
             print ("action len {0}".format(action.frame_range[1] - action.frame_range[0]))
             startFrame =  action.frame_range[0]
             endFrame = action.frame_range[1] 
+            lenAction = endFrame - startFrame
             
             isThisActionHasMarkers = len(action.pose_markers)
             
@@ -41,9 +46,13 @@ class MarkersExporter(bpy.types.Operator):
                 file.write("<animation>\n")
                 
                 for i, marker in enumerate(action.pose_markers):
-                    print ("marker index: {0} frame: {1} name: {2}".format(i, marker.frame, marker.name))
-                    file.write('    <trigger time="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / fps, marker.name))
-                
+                    if (self.normalizeMarkers == False):
+                        print ("marker index: {0} frame: {1} name: {2}".format(i, marker.frame, marker.name))
+                        file.write('    <trigger time="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / fps, marker.name))
+                    else:
+                        print ("marker index: {0} normalizedtime: {1} name: {2}".format(i, marker.frame / lenAction, marker.name))
+                        file.write('    <trigger normalizedtime="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / lenAction, marker.name))
+                        
             
                 file.write("</animation>")                 
                 file.close()
@@ -52,14 +61,19 @@ class MarkersExporter(bpy.types.Operator):
         isSceneHasMarkers = len(context.scene.timeline_markers)  
         scene = context.scene
         fps = scene.render.fps
+        lenScene = scene.frame_end
         
         if (isSceneHasMarkers):
             file = open(scene.name + "_global_markers.xml", 'wt')
             file.write("<animation>\n")
             
             for i, marker in enumerate(scene.timeline_markers):
-                file.write('    <trigger time="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / fps, marker.name))
-                            
+                if (self.normalizeMarkers == False):
+                    file.write('    <trigger time="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / fps, marker.name))
+                else:
+                    file.write('    <trigger normalizedtime="{0:.2f}" type="String" value="{1}" />\n'.format(marker.frame / lenScene, marker.name))
+                    
+                        
             file.write("</animation>")                 
             file.close()
                 
